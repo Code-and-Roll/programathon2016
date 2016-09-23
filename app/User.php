f4a7f54e04ff15f80f51580449766796dc4a39f7
Diego Guillén Hernández <dalguillen3089@gmail.com>
2016-09-22 22:56:18 -0600

    Master
    Comentario prueba de pull Diego

--- app/User.php
+++ app/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
+    /*Comentario random de Diego*/
     /**
      * The attributes that are mass assignable.
      *
