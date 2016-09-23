7311386046c93f63faf090319aea6b5996e59658
vid <vid.brenes@gmail.com>
2016-09-22 22:57:51 -0600

    prueba-vidbrenes
    …

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+/*Comment de prueba, VidBrenes*/
 
 Route::get('/user', function (Request $request) {
     return $request->user();
