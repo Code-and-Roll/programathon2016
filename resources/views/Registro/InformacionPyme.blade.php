7c085b85e9af23670aeb433ea7cd3f6a4fce305c
jjherr3ra <jjherr3ra@gmail.com>
2016-09-24 18:39:57 -0600

    Informacion Pyme, falta validad los 5k del tamanio del archivo

--- resources/views/Registro/InformacionPyme.blade.php
+++ resources/views/Registro/InformacionPyme.blade.php
@@ -2,37 +2,119 @@
 
 <div class="box box-primary">
             <div class="box-header with-border">
-              <h3 class="box-title">Quick Example</h3>
+              <h3 class="box-title">Datos de la PYME</h3>
             </div>
             <!-- /.box-header -->
             <!-- form start -->
             <form role="form">
               <div class="box-body">
                 <div class="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                  <label for="nombreComercialPYME">Nombre comercial de la PYME</label>
+                  <input type="text" class="form-control" id="nombreComercialPYME"  maxlength="100" placeholder="Ingrese el nombre comercial de la PYME" required="Debe ingresar un nombre comercial para la PYME">
                 </div>
-                <div class="form-group">
-                  <label for="exampleInputPassword1">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputFile">File input</label>
-                  <input type="file" id="exampleInputFile">
 
-                  <p class="help-block">Example block-level help text here.</p>
-                </div>
-                <div class="checkbox">
-                  <label>
-                    <input type="checkbox"> Check me out
-                  </label>
-                </div>
-              </div>
-              <!-- /.box-body -->
+                <div class="form-group">
+                  <label>Seleccione el Pais</label>
+                  <select class="form-control" required="Debe seleccionar un pais">
+                    <option>Base de datos</option>
+                    <option>Base de datos</option>
 
-              <div class="box-footer">
-                <button type="submit" class="btn btn-primary">Submit</button>
-              </div>
-            </form>
-        </div>
-          <!-- /.box -->
+                  </select>
+                </div>
+
+                <div class="form-group">
+                  <label>Seleccione la Provincia</label>
+                  <select class="form-control"  required="Debe seleccionar una provincia">
+                    <option>Base de Datos</option>
+                    <option>Base de datos</option>
+
+                  </select>
+                </div>
+
+                <div class="form-group">
+                  <label>Seleccione el Sector</label>
+                  <select class="form-control"  required="Debe seleccionar un Sector">
+                    <option>Base de Datos</option>
+                    <option>Base de datos</option>
+
+                  </select>
+                </div>
+
+                <div class="form-group">
+                  <label for="nombreComercialPYME">C&eacuote;dula Jur&iacuote;dica</label>
+                  <input type="text" class="form-control" id="nombreComercialPYME"  maxlength="50" placeholder="Ingrese la identificaci&oacute;n de la PYME" required="Debe ingresar una identificaci&oacute;n para la PYME">
+                </div>
+
+                <div class="form-group">
+                  <label>Seleccione el annio de inicio de operaciones</label>
+                  <select class="form-control"  required="Debe seleccionar un Sector" size="1">
+
+                            @for ($i=date("Y"); $i>=1900; $i--) {
+                              <option value="{{$i}}">{{$i}}</option>
+                            @endfor
+                  </select>
+                </div>
+
+                <div class="form-group">
+                  <label>Seleccione su G&eacuote;nero</label>
+                  <select class="form-control"  required="Debe seleccionar un genero">
+                    <option>Base de Datos</option>
+                    <option>Base de datos</option>
+                  </select>
+                </div>
+
+                <div class="form-group">
+                  <label for="telefono">Tel&eacute;fono de la PYME</label>
+                  <input type="text" class="form-control" id="telefono"  maxlength="50" pattern="[0-9]" placeholder="Ingrese el numero de tel&eacute;fono de la PYME" required="Debe ingresar un tel&eacuote;fono para la PYME">
+                </div>
+
+                <div class="form-group">
+                  <label for="direccion">Direcci&oacute;n de la PYME</label>
+                  <input type="text" class="form-control" id="direccion"  maxlength="200" placeholder="Ingrese la direcci&oacute;n de la PYME" required="Debe ingresar la direcci&oacute;n de la PYME">
+                </div>
+
+                @if($isActiva != 0)
+                  <div class="form-group">
+                    <div class="checkbox">
+                      <label>PYME Activa</label>
+                      <label>
+                        <input type="checkbox" value="true" name="PYMEactiva">
+                        Activa
+                      </label>
+                      <label>
+                        <input type="checkbox" value="false" name="PYMEactiva">
+                        Inactiva
+                      </label>
+                    </div>
+                  </div>
+
+                @endif
+
+                              <div class="form-group">
+                                <div class="checkbox">
+                                  <label>
+                                    <input type="checkbox" name="PYMEfamiliar">
+                                    Habilitado
+                                  </label>
+
+                                  <label>
+                                    <input type="checkbox" name="PYMEfamiliar" checked="checked">
+                                  Deshabilitado</label>
+                                </div>
+                              </div>
+
+
+                                <div class="form-group">
+                                  <label for="subirLogo">Seleccione el archivo del Logo del PYME</label>
+                                  <input type="file" id="subirLogo" accept="*.gif,*.png" >
+
+                                  <p class="help-block">Extensi&oacute;nes permitidas .gif, .png no m&aacute;s de 50 kb</p>
+                                </div>
+
+
+                              <div class="box-footer">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                              </div>
+                            </form>
+                        </div>
+                          <!-- /.box -->
